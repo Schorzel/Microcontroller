b4c9f279a856627412d0bb9eaf196a3d3d8a3dcc
DominikFutterer <61018668+DominikFutterer@users.noreply.github.com>
2020-02-19 18:41:03 +0100

    Add files via upload

---
+++ src/Alu.java
@@ -0,0 +1,41 @@
+package DateiVerarbeitung;
+
+public class Alu {
+		//Funktion zum bilden eines Zweierkomplements
+		
+	public static int zweierKomp(int value) {
+		value = value^0b11111111;
+		value++;
+		value = value& 0x00FF;
+		return value;
+	}
+	//ALU Methode welche alle arithemetischen Funktionen abdeckt
+	//p1 = 1. Rechenoperand,w=2.Rechenoperand,d = Paramater d des Befehlscode(Bit7),f== param.f befehscode(6-0),op=operator;
+	public static int ALU(int p1,int w,int d,int f, String op) {
+		
+		int value = 0;
+		 if (op.equals("&")) {
+	            value = w & p1;
+	        } else if (op.equals("|")) {
+	            value = w | p1;
+	        } else if (op.equals("^")) {
+	            value = w ^ p1;
+	        } else if (op.equals("+")) {
+	            value = w + p1;
+	        } else if (op.equals("-")) {
+	            value = p1 + zweierKomp(w);
+	        } else {
+	            value = p1;
+	        }
+	 
+	        if (value > 255) {
+	            value -= 256;
+	            cFlag = true;
+	        }
+	         
+	        saveData(d, f, value); // Speichern der Daten
+	 
+	        return value;
+	    }
+	}
+}
